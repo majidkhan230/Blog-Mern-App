26c64e5f7a1808bf3dd1148e90e2ca9572615e57
MajidKh230 <khanmajid230@gmail.com>
2025-01-20 17:46:49 +0500

    add baseURL

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 // console.log(import.meta.env.REACT_APP_API_URL)
 const apiClient = axios.create({
-    // baseURL: import.meta.env.REACT_APP_API_URL,
-    baseURL: 'http://localhost:8080/api',
+    baseURL: import.meta.env.REACT_APP_API_URL,
+    // baseURL: 'http://localhost:8080/api',
     withCredentials: true, 
     timeout: 5000,
     headers: {
